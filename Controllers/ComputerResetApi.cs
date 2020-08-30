39c032b7e8bbbb32dd3f13b947d23392d6f78884
Kevin Trinkle <kevintrinkle@Fairlane.local>
2020-08-30 10:44:36 -0500

    Removing unused Newtonsoft.

--- Controllers/ComputerResetApi.cs
+++ Controllers/ComputerResetApi.cs
@@ -1,11 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using ComputerResetApi.Models;
 using System;
