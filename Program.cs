4747c3b56692f4d461309a2229dc68691d5a0981
Kevin Trinkle <kevintrinkle@Fairlane.local>
2020-10-21 16:54:02 -0500

    Fix standby API errors...again

--- Program.cs
+++ Program.cs
@@ -13,6 +13,11 @@ namespace ComputerResetApi
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
